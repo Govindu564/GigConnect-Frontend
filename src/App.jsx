7afbf48706a8490626da26a92f3ec555ceb025e0
Govindu <govinduontipuli81@gmail.com>
2025-10-12 22:49:40 +0530

    Initialized frontend folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello! Zaalima Coders14</h1>
+      <h3>How are you guy's</h3>
+    </>
+  );
+}
+
+export default App;
